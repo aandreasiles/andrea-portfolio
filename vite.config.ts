feb79a8d8b78821532bc20a66b95b327816ad175
Andrea <silesandrea@gmail.com>
2025-07-29 15:21:14 +0200

    add title & fix route

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/andrea-portfolio",
+  base: "/andrea-portfolio/",
 })
