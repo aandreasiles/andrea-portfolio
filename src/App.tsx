98dbef3d32c34523c98949c830c8499d700a003d
Andrea <silesandrea@gmail.com>
2025-08-01 21:31:19 +0200

    refactor routing in App component to use React Router for better navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
+import './App.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import CurriculumPage from './pages/Curriculum';
 import NotFoundPage from './pages/404';
 
 export default function App() {
-  const path = window.location.pathname;
-
-  if (path === '/') {
-    return <HomePage />;
-  }
-  
-  if (path.startsWith('/portfolio')) {
-    return <CurriculumPage />;
-  }
-
-  return <NotFoundPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/portfolio" element={<CurriculumPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 }
